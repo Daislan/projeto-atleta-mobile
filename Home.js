3491ba083bf4f4d3a4d6f87efeb13dd4f9d452f2
Daislan <daislannogueiratec@gmail.com>
2023-11-04 14:29:31 -0300

    atualizações de layout

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, View, Button, Text, Image, TextInput, ScrollView, TouchableHighlight, Alert } from 'react-native';
 import axios from 'axios';
 
 export default function PesquisarAtletas({ navigation }) {
@@ -42,28 +42,89 @@ export default function PesquisarAtletas({ navigation }) {
       padding: 12,
       marginTop: 12,
       marginBottom: 16,
+      marginRight: 12,
+      flex: 1,
     },
     title: {
       fontSize: 20,
       fontWeight: 'bold',
+    },
+    cardAtleta: {
+      marginBottom: 16,
+      padding: 12,
+      backgroundColor: 'white',
+      borderRadius: 8,
+    },
+    scrollView: {
+      marginVertical: 16,
+    },
+    titleCard: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginVertical: 4,
+    },
+    imageAtleta: {
+      width: 72,
+      height: 72,
+      borderRadius: 6,
+    },
+    button: {
+      alignItems: 'center',
+      padding: 10,
+      borderWidth: 1,
+      borderRadius: 6,
+    },
+    buttonBusca: {
+      backgroundColor: '#1BC286',
+      alignItems: 'center',
+      padding: 14,
+      borderRadius: 6,
     },
   });
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pesquisa de Atletas</Text>
-      <TextInput style={styles.input}
-        placeholder="Digite o nome do atleta"
-        onChangeText={atualizarTexto}
-      />
-      <Button title="Buscar" color={'#1BC286'} onPress={buscarAtletas}/>
-      <View>
-        {resultado.map((atletaAtual, index) => (
-          <Text key={index}>
-            Nome do Atleta: {atletaAtual.player_name}
-          </Text>
-        ))}
+      <View style={{flexDirection: 'row', alignItems: 'center',}}>
+        <TextInput style={styles.input}
+          placeholder="Digite o nome do atleta"
+          onChangeText={atualizarTexto}
+        />
+        <TouchableHighlight onPress={buscarAtletas}>
+          <View style={styles.buttonBusca}>
+            <Text style={{color: 'white',}}>Buscar</Text>
+          </View>
+        </TouchableHighlight>
       </View>
+      <ScrollView style={styles.scrollView}>
+        {resultado.map((atletaAtual, index) => (
+          <View key={index} style={styles.cardAtleta}>
+            <View style={{flexDirection: 'row', alignItems: 'center',}}>
+            <Image
+              style={styles.imageAtleta} 
+              source={{
+                uri: atletaAtual.player_image,
+              }}
+            />
+            <View style={{marginLeft: 12,}}>
+              <Text style={styles.titleCard}>
+                {atletaAtual.player_name}
+              </Text>
+              <Text>
+                {atletaAtual.team_name}
+              </Text>
+            </View>
+            </View>
+            <View style={{marginTop: 12,}}>
+              <TouchableHighlight onPress={''}>
+                  <View style={styles.button}>
+                    <Text>Adicionar aos favoritos</Text>
+                  </View>
+              </TouchableHighlight>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
