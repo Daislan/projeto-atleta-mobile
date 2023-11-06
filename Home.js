ce9eee940b192b5c696e3fe90d163deceff7676e
Daislan <daislannogueiratec@gmail.com>
2023-11-06 15:27:25 -0300

    melhoria no botao favoritos

--- Home.js
+++ Home.js
@@ -72,6 +72,7 @@ export default function PesquisarAtletas({ navigation }) {
       padding: 10,
       borderWidth: 1,
       borderRadius: 6,
+      backgroundColor: '#FFFFFF',
     },
     buttonBusca: {
       backgroundColor: '#1BC286',
